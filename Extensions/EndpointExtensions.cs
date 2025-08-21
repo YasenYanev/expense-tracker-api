1af92eec95d11589e8a2eb0c0375a88869cdabbe
Yasen Yanev <124697579+YasenYanev@users.noreply.github.com>
2025-08-21 16:02:11 +0300

    refactor: move endpoint mappings to extension method

---
+++ Extensions/EndpointExtensions.cs
@@ -0,0 +1,33 @@
+namespace expense_tracker_api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using expense_tracker_api.Data;
+using expense_tracker_api.Services.Interfaces;
+using expense_tracker_api.Dtos;
+
+public static class EndpointExtensions
+{
+    public static void MapEndpoints(this WebApplication app)
+    {
+        app.MapGet("/", async (ExpensesDbContext dbContext) =>
+        {
+            var dbTest = await dbContext.ExpensesCategories.ToListAsync();
+            return Results.Json(dbTest);
+        });
+
+        app.MapPut("/register", () =>
+        {
+            // TODO: Store users in database and authenticate them
+        });
+
+        app.MapPost("/login", (IJWTServiceTest jwtServiceTest4e, HttpContext context, UserDto user) =>
+        {
+            var username = user.Username;
+            var role = user.Role;
+
+            if (!(username == "Yasen" && role == "Admin")) return Results.Unauthorized();
+
+            context.Response.Headers["Authorization"] = jwtServiceTest4e.GenerateToken(username, role);
+            return Results.Ok("Jwt Token Created!");
+        });
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using expense_tracker_api.Data;
-using expense_tracker_api.Services.Interfaces;
 using expense_tracker_api.Extensions;
-using expense_tracker_api.Dtos;
-
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ExpensesDB");
@@ -13,24 +8,5 @@ var secretKey = builder.Configuration["Jwt:Key"];
 builder.Services.AddServices(connectionString!, secretKey!);
 
 var app = builder.Build();
-
-app.MapGet("/", async (ExpensesDbContext dbContext) =>
-{
-    var dbTest = await dbContext.ExpensesCategories.ToListAsync();
-    return Results.Json(dbTest);
-});
-
-
-app.MapPost("/login", ([FromServices] IJWTServiceTest jwtServiceTest4e, HttpContext context, UserDto user) =>
-{
-    var username = user.Username;
-    var role = user.Role;
-
-    if (!(username == "Yasen" && role == "Admin")) return Results.Unauthorized();
-
-    context.Response.Headers["Authorization"] = jwtServiceTest4e.GenerateToken(username, role);
-    return Results.Ok("Jwt Token Created!");
-});
-
-
+app.MapEndpoints();
 app.Run();
